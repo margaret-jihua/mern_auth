622e92126025b0e75cfa46a3867852c383c8f92e
margare-jihua <margaret.jihua@gmail.com>
2020-09-03 10:39:09 -0700

    [User] Add model and export

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+const MONGO_URI = process.env.MONGO_URI
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: true
